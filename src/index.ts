afbfd71ffa957d0fff0205347996d92b01c6404d
hjPark <cool.itea@gmail.com>
2019-03-03 12:07:46 +0900

    Using Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
+const lucy = {
   name: "Lucy",
   age: 36,
   gender: "female"
 };
 
 const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age} and a ${person.gender}!`;
+  return `Hello ${person.name}, you are ${person.age} and a ${person.gender}.`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lucy));
